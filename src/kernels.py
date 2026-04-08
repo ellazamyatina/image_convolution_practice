aa65f098ef61b2b09e6f9314c5498033aadfe242
Ella Zamyatina <230418248+ellazamyatina@users.noreply.github.com>
2026-04-08 16:28:18 +0300

    Make class and add the most common kernels

---
+++ src/kernels.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Kernel:
+    def __init__(self,matrix):
+        if isinstance(matrix,list):
+            matrix = np.array(matrix, dtype=np.float32)
+        self.matrix = matrix
+        self.shape = matrix.shape
+    def __call__(self):
+        return self.matrix
+    def __repr__(self):
+        return f"Kernel{self.shape}"
+    @classmethod
+    def blur(cls):
+        return cls([[1/9,1/9,1/9],
+                    [1/9,1/9,1/9],
+                    [1/9,1/9,1/9]])
+    @classmethod
+    def sharpen(cls):
+        return cls([[0,-1,0],
+                    [-1,5,-1],
+                    [0,-1,0]])
+    @classmethod
+    def gaussian(cls):
+        return cls([[1/16,2/16,1/16],
+                    [2/16,4/16,2/16],
+                    [1/16,2/16,1/16]])
+    
