45cdd2fbc9a2aaa3dfc888206a95772bf30361b5
Ella Zamyatina <230418248+ellazamyatina@users.noreply.github.com>
2026-05-27 17:55:46 +0300

    Fix: support grayscale and extra argument for zero padding

--- src/padding.py
+++ src/padding.py
@@ -8,29 +8,32 @@ PADDING_MODES = {
 }
 
 
-def apply_padding(image: np.ndarray,
-                  kernel_shape: tuple[int, int],
-                  mode: str = "zero") -> np.ndarray:
+def apply_padding(
+    image: np.ndarray, kernel_shape: tuple[int, int], mode: str = "zero"
+) -> np.ndarray:
     if mode not in PADDING_MODES:
         raise ValueError(
-            f"Unknown padding mode: {mode}. "
-            f"Available: {list(PADDING_MODES.keys())}"
+            f"Unknown padding mode: {mode}. Available: {list(PADDING_MODES.keys())}"
         )
 
     m, n = kernel_shape
     padding_height = m // 2
     padding_width = n // 2
 
-    #if the image is grayscale => r=g=b
-    pad_width = (
+    pad_width = [
         (padding_height, padding_height),
         (padding_width, padding_width),
-        (0, 0),
-    )
+    ]
+    if image.ndim == 3:
+        pad_width.append((0, 0))
+
+    kwargs = {}
+    if mode == "zero":
+        kwargs["constant_values"] = 0
 
     return np.pad(
         image,
         pad_width,
         mode=PADDING_MODES[mode],
-        constant_values=0,
+        **kwargs,
     )
